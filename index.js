3e80d8e9ddba6e6ca26cd3b80d70ff2121ac0b92
KaanOzen-mF <kaanozn98@gmail.com>
2023-08-17 22:50:52 +0300

    Arama özelliği için olan maddelerin 3'ü tamamlandır

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const oneWayRadio = document.getElementById("one-way");
 const roundTripRadio = document.getElementById("round-trip");
 const returnDateInput = document.getElementById("return");
 const arrivialDateInput = document.getElementById("arrivial-return");
-
+const searchForm = document.getElementById("search-form");
 const fromInput = document.getElementById("ticket-from");
 const toInput = document.getElementById("ticket-to");
 
@@ -16,29 +16,67 @@ roundTripRadio.addEventListener("change", () => {
   arrivialDateInput.disabled = false;
 });
 
-fetch("https://64de0905825d19d9bfb1ec57.mockapi.io/flightsData/airlineData")
+const fromBox = document.getElementById("from-box");
+const toBox = document.getElementById("to-box");
+
+const fromResultBox = document.querySelector(".from-result");
+const toResultBox = document.querySelector(".to-result");
+
+const airportData = fetch(
+  "https://64de0905825d19d9bfb1ec57.mockapi.io/flightsData/airlineData"
+)
   .then((res) => res.json())
-  .then((airlineData) => {
-    const searchAndUpdateFrom = () => {
-      const fromValue = fromInput.value.toLowerCase();
-      const searchResults = airlineData.filter((ticket) =>
-        ticket.from.toLowerCase().includes(fromValue)
+  .then((data) => {
+    fromBox.onkeyup = function () {
+      let input = fromBox.value.toLowerCase();
+      let result = data.filter((airport) =>
+        airport.from.toLowerCase().includes(input)
       );
-      console.log(searchResults);
-    };
-    const searchAndUpdateTo = () => {
-      const toValue = toInput.value.toLowerCase();
-      const searchResults = airlineData.filter((ticket) =>
-        ticket.from.toLowerCase().includes(toValue)
-      );
-      console.log(searchResults);
+      result = removeDuplicates(result, "from");
+      display(result, "from");
     };
 
-    fromInput.addEventListener("input", searchAndUpdateFrom);
-    toInput.addEventListener("input", searchAndUpdateTo);
+    toBox.onkeyup = function () {
+      let input = toBox.value.toLowerCase();
+      let result = data.filter((airport) =>
+        airport.from.toLowerCase().includes(input)
+      );
+      result = removeDuplicates(result, "from");
+      display(result, "to");
+    };
   });
 
-const searchForm = document.getElementById("search-form");
+function removeDuplicates(arr, key) {
+  const seen = new Set();
+  return arr.filter((item) => {
+    const value = item[key];
+    if (!seen.has(value)) {
+      seen.add(value);
+      return true;
+    }
+    return false;
+  });
+}
+
+function display(result, type) {
+  const resultBox = type === "from" ? fromResultBox : toResultBox;
+
+  const content = result.map((airport) => {
+    return `<li onclick="selectInput('${type}', this)">${airport.from}</li>`;
+  });
+
+  resultBox.innerHTML = `<ul>${content.join("")}</ul>`;
+}
+
+function selectInput(type, list) {
+  if (type === "from") {
+    fromBox.value = list.innerHTML;
+    fromResultBox.innerHTML = "";
+  } else if (type === "to") {
+    toBox.value = list.innerHTML;
+    toResultBox.innerHTML = "";
+  }
+}
 
 searchForm.addEventListener("submit", (event) => {
   event.preventDefault(); // Prevent the form from submitting
@@ -47,8 +85,8 @@ searchForm.addEventListener("submit", (event) => {
   const returnInput = document.getElementById("return").value;
   const arrivialStartInput = document.getElementById("arrivial-start").value;
   const arrivialReturnInput = document.getElementById("arrivial-return").value;
-  const ticketFromInput = document.getElementById("ticket-from").value;
-  const ticketToInput = document.getElementById("ticket-to").value;
+  const ticketFromInput = document.getElementById("from-box").value;
+  const ticketToInput = document.getElementById("to-box").value;
 
   console.log("Ticket From:", ticketFromInput);
   console.log("Ticket From:", ticketToInput);
